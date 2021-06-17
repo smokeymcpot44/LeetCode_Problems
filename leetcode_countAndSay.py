121b0faa45ff243db054e2d4efd60cf06d5b98f8
smokeymcpot44 <85619932+smokeymcpot44@users.noreply.github.com>
2021-06-17 12:54:01 +0530

    Updated comments

--- leetcode_countAndSay.py
+++ leetcode_countAndSay.py
@@ -1,25 +1,35 @@
+
+
 def countAndSay(n):
         for i in range(n - 1):
+            # initilazing the final string for the first iteration only
             if i == 0:
                 raw = str(1)
-            raw_list = [word for word in raw]
+            raw_list = [word for word in raw] 
             new_l = []
             num = 0
-            while num < len(raw_list):
+            while num < len(raw_list): 
                 try:
-                    rw = raw_list[num]
-                    while True:
-                        if raw_list[num] == raw_list[num+1]:
-                            rw += raw_list[num]
-                            num += 1
+                    rw = raw_list[num]  # initializing the string for similar next character
+                    while True: 
+                        if raw_list[num] == raw_list[num+1]:  # check if the next character is same as the current
+                            rw += raw_list[num] # adding it to the string of similar characters
+                            num += 1 # increase counter
                         else:
-                            num += 1
+                            # if the next character is not same, increase the counter so that it moves to next character
+                            # break the loop to make a fresh string of similar characters
+                            num += 1 
                             break
-                    new_l.append(rw)
+                    new_l.append(rw) # add the string of similar characters to a list
                 except IndexError:
+                     # exception for last index of the list
+                     # occurs when the last characters is compared to a non-existent next character
+                     # adds the last element to the list if not previously added with last similar string
                     new_l.append(rw)
                     break
-            raw = "".join([str(len(ty))+ty[0] for ty in new_l])
+            # calculates the length of every similar string
+            # combines the count of the character and the character
+            raw = "".join([str(len(element)) + element[0] for element in new_l]) 
 
         return "1" if n == 1 else raw
         
